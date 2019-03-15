f2be64851fb7948d9f7a0500a24dcc9d6b5bedd6
Roy <root@mrsheldon.me>
2019-03-15 13:50:51 +0200

    Add files via upload

---
+++ discord.php
@@ -0,0 +1,119 @@
+<html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><title>MrSheldon | Discord</title><meta name="description" content="MrSheldon - Full Stack Web Developer"> <meta name="author" content="MrSheldon"> <meta name="twitter:card" value="summary"> <meta name="twitter:title" content="MrSheldon"> <meta name="twitter:description" content="MrSheldon - Full Stack Web Developer"> <meta name="twitter:image" content="https://mrsheldon.me/assets/images/mrsheldon.png"> <meta name="theme-color" content="#000000"> <meta property="og:site_name" value="MrSheldon.me"> <meta property="og:title" content="MrSheldon"> <meta property="og:url" content="https://mrsheldon.me"> <meta property="og:description" content="MrSheldon - Full Stack Web Developer"> <meta property="og:image" content="https://mrsheldon.me/assets/images/mrsheldon.png"> <link rel="icon" href="https://mrsheldon.me/assets/images/mrsheldon.png"> <link rel="shortcut icon" href="https://mrsheldon.me/assets/images/mrsheldon.png"><link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet"></head><body><style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto; 
+  width: 200; 
+  height: 200;
+}
+* {
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+}
+
+body {
+  background: #23272A !important;
+  padding: 0;
+  margin: 0;
+}
+
+#discordpage {
+  position: relative;
+  height: 100vh;
+}
+
+#discordpage .discordpage {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+}
+
+.discordpage {
+  max-width: 767px;
+  width: 100%;
+  line-height: 1.4;
+  padding: 0px 15px;
+}
+
+.discordpage .discordpage-0 {
+  position: relative;
+  height: 150px;
+  line-height: 150px;
+  margin-bottom: 25px;
+}
+
+.discordpage .discordpage-0 h1 {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 100px;
+  font-weight: 900;
+  margin: 0px;
+  background-position: center;
+}
+
+.discordpage h2 {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 26px;
+  font-weight: 700;
+  margin: 0;
+  text-align: center;
+  color: #fff
+}
+
+.discordpage p {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  margin-bottom: 0px;
+  text-transform: uppercase;
+}
+
+.discordpage a {
+  font-family: 'Titillium Web', sans-serif;
+  display: inline-block;
+  text-transform: uppercase;
+  color: #fff;
+  text-decoration: none;
+  border: none;
+  background: #7289DA;
+  padding: 10px 40px;
+  font-size: 14px;
+  font-weight: 700;
+  border-radius: 1px;
+  margin-top: 15px;
+  -webkit-transition: 0.2s all;
+  transition: 0.2s all;
+}
+
+.discordpage a:hover {
+  opacity: 0.8;
+}
+
+@media only screen and (max-width: 767px) {
+  .discordpage .discordpage-0 {
+    height: 110px;
+    line-height: 110px;
+  }
+  .discordpage .discordpage-0 h1 {
+    font-size: 120px;
+  }
+}
+.blurple {
+    color: #7289DA !important;
+}
+</style>
+
+<div id="discordpage">
+<div class="discordpage">
+    <img src="https://discordapp.com/assets/f8389ca1a741a115313bede9ac02e2c0.svg" class="center">
+   
+<div class="discordpage-0">
+<h1 class="blurple">MrSheldon#0001</h1>
+</div>
+<h2>Add me on Discord</h2>
+<p style="text-align: center;"><a href="/">Go To Homepage</a></p></div>
+</div>
+
+</body></html>
