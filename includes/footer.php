accee0001df1a651b949c00cc99d3df737f0421f
Roee Lupo <root@mrsheldon.me>
2020-08-04 02:14:38 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,1 @@
-<p class="copyright">Copyright © 2018-<?php echo date("Y"); ?> <a data-toggle="tooltip" title="Also known as MrSheldon" href="https://mrsheldon.me" targe="_blank">Roee Lupo</a>, All Rights Reserved.</p>
+<p class="copyright">Copyright © 2018-<?php echo date("Y"); ?> <a data-toggle="tooltip" title="Also known as MrSheldon" href="https://roeelupo.com" targe="_blank">Roee Lupo</a>, All Rights Reserved.</p>
