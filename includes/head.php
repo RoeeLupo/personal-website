2c8b834e222aaa7665462cbb5c8ceb0043c5e369
Roee Lupo <root@mrsheldon.me>
2020-08-04 02:14:07 +0300

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -3,16 +3,16 @@
 <title><?= $CURRENT_PAGE; ?></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="MrSheldon - Full stack web developer and IT technician from Israel" />
-<meta name="author" content="MrSheldon">
+<meta name="description" content="Roee Lupo - Full stack web developer and IT technician from Israel" />
+<meta name="author" content="Roee Lupo">
 <meta name="twitter:card" value="summary" />
-<meta name="twitter:title" content="MrSheldon" />
-<meta name="twitter:description" content="MrSheldon - Full stack web developer and IT technician from Israel" />
+<meta name="twitter:title" content="Roee Lupo" />
+<meta name="twitter:description" content="Roee Lupo - Full stack web developer and IT technician from Israel" />
 <meta name="theme-color" content="#000000">
-<meta property="og:site_name" value="MrSheldon.me">
-<meta property="og:title" content="MrSheldon" />
-<meta property="og:url" content="https://mrsheldon.me" />
-<meta property="og:description" content="MrSheldon - Full stack web developer and IT technician from Israel" />
+<meta property="og:site_name" value="Roee Lupo.me">
+<meta property="og:title" content="Roee Lupo" />
+<meta property="og:url" content="https://roeelupo.me" />
+<meta property="og:description" content="Roee Lupo - Full stack web developer and IT technician from Israel" />
 <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
